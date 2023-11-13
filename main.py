e3efec2466cd000108e332611db564fd410dacd2
Bea gyeong seok <gsr025silver.naver.com>
2023-11-13 11:33:12 +0900

    baekjoon 2566 최대값 https://www.acmicpc.net/problem/2566

--- main.py
+++ main.py
@@ -2,15 +2,11 @@
 baekjoon 2566 최대값
 https://www.acmicpc.net/problem/2566
 """
-max = 0
-col, row = 1,1
 
-for i in range(9):
-    list = input().split()
-    for j in range(9):
-        if (max < int(list[j])):
-            max = int(list[j])
-            col = i + 1
-            row = j + 1
-print(max)
-print(col,row)
+bowl = []
+for i in range(5):
+    bowl.append(list(input()))
+for i in range(15):
+    for j in range(5):
+        if i < len(bowl[j]):
+            print(bowl[j][i],end="")
