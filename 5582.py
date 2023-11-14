677fbe05a0db58f9120b8a0f40f79cef944e0599
ukkji coding <byunguk98@naver.com>
2023-11-14 15:10:21 +0900

    2023.11.14 백준 5582번(수정). 좀 더 고민해볼 것.

--- 5582.py
+++ 5582.py
@@ -1,24 +1,15 @@
 word1 = input()
 word2 = input()
 
-word1_substrings = set([word1[i:j+1] for i in range(len(word1)) for j in range(i, len(word1))])
-word2_substrings = set([word2[i:j+1] for i in range(len(word2)) for j in range(i, len(word2))])
+prev = [0] * (len(word2) + 1)
+answer = 0
 
-common_substring = []
-for i in range(len(word1_substrings)):
-    for j in range(len(word2_substrings)):
-        if list(word1_substrings)[i] == list(word2_substrings)[j]:
-            common_substring.append(list(word2_substrings)[j])
-        else:
-            continue
+for i in range(len(word1)):
+    tmp = [0] * (len(word2) + 1)
+    for j in range(len(word2)):
+        if word1[i] == word2[j]:
+            tmp[j + 1] = prev[j] + 1
+    answer = max(answer, max(tmp))
+    prev = tmp[:]
 
-if len(common_substring) != 0:
-    max_str = common_substring[0]
-    for i in range(1, len(common_substring)):
-        if len(max_str) < len(common_substring[i]):
-            max_str = common_substring[i]
-    print(len(max_str))
-
-else:
-    print("0")
-
+print(answer)
