fba26e6f0bd1b8d0e3e04d0b86b8f7d6058bd9b9
Bea gyeong seok <gsr025silver.naver.com>
2023-11-21 01:51:16 +0900

    baekjoon 1427 소트인사이드 https://www.acmicpc.net/problem/1427

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 """
-baekjoon 10989 수 정렬하기 3
-https://www.acmicpc.net/problem/10989
-
-카운팅 정렬을 이용해 해결했다.
+baekjoon 1427 소트인사이드
+https://www.acmicpc.net/problem/1427
 """
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-nums = [0] * 10000
-
-for i in range(N):
-    num = int(input())
-    nums[num-1] += 1
-
-for i in range(10000):
-    for j in range(nums[i]):
-        print(i + 1)
+Nlen = len(str(N))
+for i in range(9,-1,-1):
+    num = N
+    count = 1
+    while num != 0:
+        pn = num%10
+        num = int(num/10)
+        if pn == i:
+            print(pn,end="")
+            tmp = int(N / (10**count))
+            N %= 10**(count-1)
+            N = N + tmp * (10**(count-1))
+            count -= 1
+            Nlen -= 1
+        count += 1;
+for i in range(Nlen):
+    print(0,end="")
