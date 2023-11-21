35ba42f3ca34a48f4c796f53d582d5e1fdfe9a6e
ukkji coding <byunguk98@naver.com>
2023-11-21 23:52:07 +0900

    2023.11.21 LeetCode 75번(수정). 버블 정렬 알고리즘 사용. 시간 복잡도는 모든 경우에 O(N^2)로 동일.

--- 75.py
+++ 75.py
@@ -1,8 +1,10 @@
 class Solution:
     def sortColor(self, color_list):
-        res = sorted(color_list)
-        print(res)
-        return res
+        for i in range(len(color_list)):
+            for j in range(1, len(color_list)-i):
+                if color_list[j-1] > color_list[j]:
+                    color_list[j], color_list[j-1] = color_list[j-1], color_list[j]
+        return color_list
 
 a = Solution()
 a.sortColor([2, 0, 2, 1, 1, 0])
