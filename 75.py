1695a7037097ab4d97c926f97b1b245c1a021319
ukkji coding <byunguk98@naver.com>
2023-11-22 00:30:12 +0900

    2023.11.22 LeetCode 75번(2차 수정, 다른 답안). 삽입 정렬 사용, 시간 복잡도 : 이미 정렬되어 있는 경우에는 O(N), 그 외의 경우에는 O(N^2)

--- 75.py
+++ 75.py
@@ -1,9 +1,10 @@
 class Solution:
     def sortColor(self, color_list):
         for i in range(len(color_list)):
-            for j in range(1, len(color_list)-i):
-                if color_list[j-1] > color_list[j]:
-                    color_list[j], color_list[j-1] = color_list[j-1], color_list[j]
+            for j in range(i-1, -1, -1):
+                if color_list[j+1] <= color_list[j]:
+                    color_list[j+1], color_list[j] = color_list[j], color_list[j+1]
+        print(color_list)
         return color_list
 
 a = Solution()
