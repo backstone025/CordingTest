3020973261b7c52d5a9a7210a75dbebe9bbc3f9a
ukkji coding <byunguk98@naver.com>
2023-11-24 14:15:52 +0900

    2023.11.24 백준 10986번(수정). 시간 복잡도 : O(N)

--- 10986.py
+++ 10986.py
@@ -4,14 +4,25 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
-cnt = 0
-for i in range(N):
-    sum = 0
-    for j in range(i, N):
-        sum += A[j]
-        if sum % M == 0:
-            cnt += 1
-        else:
-            continue
+sum = [0] * N
+cnt = [0] * M
 
-print(cnt)
+sum[0] = A[0]
+ans = 0
+
+for i in range(1, N):
+    sum[i] = sum[i-1] + A[i]
+
+for i in range(N):
+    remind = sum[i] % M
+    if remind == 0:
+        ans += 1
+
+    cnt[remind] += 1
+
+for i in range(M):
+    if cnt[i] > 1:
+        ans += (cnt[i] * (cnt[i] - 1) // 2)
+
+print(ans)
+
