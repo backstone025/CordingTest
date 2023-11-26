6a55c9bf35095c4ce6dd8edfc0f53d5940e9cc3e
Bea gyeong seok <gsr025silver.naver.com>
2023-11-26 17:33:15 +0900

    baekjoon 2667 단지번호붙이기 https://www.acmicpc.net/problem/2667
    브루트 포스 -> 단지 탐색('1'인 곳 찾기){
        BFS -> 단지 개수 세기{
        }
    }

--- main.py
+++ main.py
@@ -1,42 +1,62 @@
 """
-baekjoon 2606 바이러스
-https://www.acmicpc.net/problem/2606
+baekjoon 2667 단지번호붙이기
+https://www.acmicpc.net/problem/2667
 
-연습할 겸 DFS를 이용해 구현했다.
+브루트 포스 -> 단지 탐색('1'인 곳 찾기){
+    BFS -> 단지 개수 세기{
+    }
+}
 """
 import sys
+
 input = sys.stdin.readline
 from collections import deque
 
-def DFS(es):
-    visited = [1]
-    stack = [None, 1]
-    pos = 1
-    while stack and not pos is None:
-        for end in es[pos]:
-            if end not in visited:
-                es[pos].popleft()
-                visited.append(end)
-                stack.append(end)
-                pos = end
-                break
-        else:
-            stack.pop()
-            pos = stack[-1]
-    return len(visited)-1
+
+def findNeighbor(pos, n):
+    x = [1, 0, -1, 0]
+    y = [0, 1, 0, -1]
+    ns = []
+    for i in range(4):
+        r, c = pos[0] + x[i], pos[1] + y[i]
+        if (c >= 0 and c < n) and (r >= 0 and r < n):
+            ns.append([r, c])
+    return ns
 
 
-n_num = int(input())
-e_num = int(input())
+def search(m, n):
+    Visited = ([])
+    result = []
+    for r in range(n):
+        for c in range(n):
+            pos = [r, c]
+            if pos not in Visited and m[pos[0]][pos[1]]:
+                count = 1
+                queue = deque([pos])
+                Visited.append(pos)
+                while queue:
+                    pos = queue.popleft()
+                    m[pos[0]][pos[1]] = False
+                    for b in findNeighbor(pos, n):
+                        if m[b[0]][b[1]]:
+                            count += 1
+                            queue.append(b)
+                            Visited.append(b)
+                            m[b[0]][b[1]] = False
+                result.append(count)
+            else:
+                Visited.append(pos)
 
-inputs = []
-for _ in range(e_num):
-    h, t = map(int, input().split())
-    inputs.append((h, t))
-    inputs.append((t, h))
-inputs.sort()
-edges = {i+1 : deque([]) for i in range(n_num)}
-for t in inputs:
-    edges[t[0]].append(t[1])
+    return sorted(result)
 
-print(DFS(edges))
+
+N = int(input())
+metrix = []
+for i in range(N):
+    r = [True if i == '1' else False for i in input()]
+    metrix.append(r)
+
+builds = search(metrix, N)
+print(len(builds))
+for i in builds:
+    print(i)
