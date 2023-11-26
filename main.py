c3199dc84d98754aab086b1a7769f52cd8d445c7
Bea gyeong seok <gsr025silver.naver.com>
2023-11-27 02:42:56 +0900

    baekjoon 11724 연결 요소의 개수 https://www.acmicpc.net/problem/11724
    맨 처음 값을 넣어줄 경우를 따지지 않아 문제가 발생했다.
    맨 처음 queue값 넣을 때 node의 개수가 충분한지(pop()에러 방지)
    만약 그렇다면 넣는 동시에 nodes에서 빼주기

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 """
-baekjoon 1697 숨박꼭질
-https://www.acmicpc.net/problem/1697
+baekjoon 11724 연결 요소의 개수
+https://www.acmicpc.net/problem/11724
 
-방문 여부 확인할 땐 list말고 set을 쓰자.
-시간 에러가 여러번 발생했는데, 원인은 전부 방문 여부 확인하는데 시간이 걸려서였다.
-list[] : O(n) -> 순차적인 모든 요소 확인
-set{} : O(1) -> 해시 테이블로 구현되어 있어서
+맨 처음 값을 넣어줄 경우를 따지지 않아 문제가 발생했다.
+맨 처음 queue값 넣을 때 node의 개수가 충분한지(pop()에러 방지)
+만약 그렇다면 넣는 동시에 nodes에서 빼주기
 """
-import sys
 
+import sys
 input = sys.stdin.readline
+
 from collections import deque
 
+N, M = map(int, input().split())
+nodes = {i for i in range(1, N+1)}
+edges = {i : [] for i in range(1, N+1)}
+for i in range(M):
+    u, v = map(int, input().split())
+    edges[u].append(v)
+    edges[v].append(u)
 
-def chaseTime(c, r):
-    pos = [c, 0]
-    queue = deque([pos])
-    visited = {c}
-    while queue:
-        if pos[0] == r:
-            break
-        pos = queue.popleft()
-        pos[1] += 1
-        next = [pos[0] + 1, pos[0] * 2, pos[0] - 1]
-        for n in next:
-            if n >= 0 and n <= 100000 and n not in visited:
-                queue.append([n, pos[1]])
-                visited.add(n)
-                if n == r:
-                    pos = [n, pos[1]]
-                    break
-    return pos[1]
-
-
-c, r = map(int, input().split())
-print(chaseTime(c, r))
+queue = deque([])
+if len(nodes) >= 1:
+    queue.append(nodes.pop())
+count = 1
+while nodes:
+    n = queue.popleft()
+    for k in edges[n]:
+        if k in nodes:
+            nodes.remove(k)
+            queue.append(k)
+    if len(queue) == 0:
+        count += 1
+        queue.append(nodes.pop())
+print(count)
